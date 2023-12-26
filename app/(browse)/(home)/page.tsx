ec49f3aafc93c4c2f987931d9a3b69766a4fcc89
yzy98 <yz186337@163.com>
2023-12-26 16:52:04 +0800

    First commit

---
+++ app/(browse)/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <p className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </p>
+  );
+}
