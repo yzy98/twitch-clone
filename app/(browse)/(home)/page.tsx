f8bcb137ee8288a1593856de5184f1320648c2e1
yzy98 <yz186337@163.com>
2023-12-28 15:14:45 +0800

    Add: Navbar component

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
     <p className="flex flex-col gap-y-4">
-      <h1>Dashboard</h1>
-      <UserButton afterSignOutUrl="/" />
+      <h1>Home page</h1>
     </p>
   );
 }
